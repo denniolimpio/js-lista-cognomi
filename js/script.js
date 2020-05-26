9f698d2bc32295d4c1c4f2ae3fece56b76bc1cba
dennis <dennis.olimpio0794@gmail.com>
2020-05-27 00:50:27 +0200

    ho aggiunto alcuni commenti

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // TESTO ESERCIZIO
-
 // Lista Cognomi
 // Chiedere all’utente il cognome
 // inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
+
 
 // SVOLGIMENTO ESERCIZIO
 
@@ -13,35 +13,34 @@ var list  = ['Adamo', 'Zaccaro', 'Baldi', 'Biondi', 'Verdi', 'Mazza'];
 console.log ("Lista di partenza: " + list);
 
 for ( var i = 0; i < list.length; i++) {
-  // console.log ( list[i] );
 
 }
 
 // variabili
 var posizione;
 
-// chiedo all'utente di inserire il cognome
 
 // aggiungo validazione
+
 do {
 
+  // chiedo all'utente di inserire il cognome
   var cognomeUtente = prompt (' Inserisci il tuo cognome');
 
 } while ( cognomeUtente <= 0) {
 
 }
 
-
 // aggiungo cognome alla lista
-list.push (cognomeUtente);
 
+list.push (cognomeUtente);
 
 // ordino alfabeticamente elementi lista
 
 list.sort()
 console.log("Lista aggiornata e ordinata: " + list);
 
-// Stampo a schermo
+// posizione cognome utente
 
 var posizione = list.indexOf(cognomeUtente);
 console.log( "Posizione elemento utente:  " + (posizione + 1));
